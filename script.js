5aa0592ec88879cb35172e834a84c64d33aec68e
Ash <44040571+Asghar2018@users.noreply.github.com>
2019-05-08 16:24:29 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,54 @@
+function changeTheme(textColor, backgroundColor, fontName) {
+    // use jQuery to set the page text color to the textColor parameter
+    
+    // use jQuery to set the page background color to the backgroundColor parameter
+    
+    // use jQuery to set the page font to the fontName parameter
+    
+}
+
+// In  each of the click functions below, replace the 3 lines of jquery with a function call to changeTheme
+$("#theme1").click(function(){
+    $("body").css("color", "red");
+    $("body").css("background-color", "pink");
+    $("body").css("font-family", "Abril Fatface");
+});
+
+$("#theme2").click(function(){
+    $("body").css("color", "violet");
+    $("body").css("background-color", "lavender");
+    $("body").css("font-family", "Pacifico");
+});
+
+$("#theme3").click(function(){
+    $("body").css("color", "DarkOrange");
+    $("body").css("background-color", "Khaki");
+    $("body").css("font-family", "Gugi");
+});
+
+$("#theme4").click(function(){
+    $("body").css("color", "yellow");
+    $("body").css("background-color", "coral");
+    $("body").css("font-family", "Indie Flower");
+});
+
+$("#theme5").click(function(){
+    $("body").css("color", "blue");
+    $("body").css("background-color", "LightSteelBlue");
+    $("body").css("font-family", "Just Another Hand");
+});
+
+$("#theme6").click(function(){
+    $("body").css("color", "indigo");
+    $("body").css("background-color", "thistle");
+    $("body").css("font-family", "Modak");
+});
+
+$("#theme7").click(function(){
+    $("body").css("color", "green");
+    $("body").css("background-color", "YellowGreen");
+    $("body").css("font-family", "Cormorant Unicase");
+});
+
+
+
